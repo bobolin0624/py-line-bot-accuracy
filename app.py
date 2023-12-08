10c60f7898387391f84929b59abe7a1b59a14739
casey.lin <casey.lin@pioneerm.com>
2023-12-08 16:35:00 +0800

    feat: add stock query function test

--- app.py
+++ app.py
@@ -24,21 +24,33 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     user_message = event.message.text
-    r = requests.get('https://tw.rter.info/capi.php')
-    currency = r.json()
+    currency_request = requests.get('https://tw.rter.info/capi.php')
+    currency = currency_request.json()
 
     currencyDate = currency['USDTWD']['UTC']
-
-    if user_message == '日幣' or user_message ==  'JPY' or user_message ==  'jpy':
+    # stock
+    stock_num = user_message.split(' ', 1 )
+    print(stock_num)
+    if stock_num[0] == 'stock':
+        stock_request = requests.get(f'https://mis.twse.com.tw/stock/api/getStock.jsp?ch={stock_num[1]}.tw')
+        stock_information = stock_request.json()
+        stock_name = stock_num[1]
+        stock_price = stock_information['msgArray']
+        print(stock_price)
+        msg = f'you want know stock {stock_name} now is {stock_price}'
+    # exchange currency
+    if user_message ==  'JPY' or user_message ==  'jpy':
         currencyQueryJP = currency['USDJPY']
         currencyQueryTW = currency['USDTWD']
         finalCurrency = currencyQueryTW['Exrate'] /currencyQueryJP['Exrate']
+        msg = f"{currencyDate} exchange accurate = {round(finalCurrency, 4)}"
         
-    elif user_message == '美金' or user_message == 'USD' or user_message == 'usd':
+    elif user_message == 'USD' or user_message == 'usd':
         currencyQuery = currency['USDTWD']
         finalCurrency = currencyQuery['Exrate']
+        msg = f"{currencyDate} exchange accurate = {round(finalCurrency, 4)}"
 
-    msg = f"{currencyDate} exchange accurate = {round(finalCurrency, 4)}"
+    
     reply_text = msg
     line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
 
